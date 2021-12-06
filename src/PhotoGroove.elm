c8eb70b4cd1101bb515159a37f4d387d60de261e
Urs Alexandru-Tiberiu <alexandru.urs@fortech.ro>
2021-12-06 21:58:54 +0200

    Initial commit

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,1 +1,16 @@
-module Main exposing (..)
+module PhotoGroove exposing (..)
+
+import Html exposing (h1, text)
+
+
+main =
+    view
+
+
+view =
+    h1 [] [ text "Hello, world" ]
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
